8e9dfb17bdb764d2adbd838d6bb05cefdbadbedb
shal1opie <rasim.2003.aliev@mail.ru>
2023-12-01 01:49:37 +0300

    дополнил изученным материалом

--- django/DiplomDjango/DiplomDjangoCFDB/admin.py
+++ django/DiplomDjango/DiplomDjangoCFDB/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import AppTypes,People,Roles,RolesRaisers,SpaceAchiv,Users
 
-# Register your models here.
+admin.site.register(AppTypes)
+admin.site.register(People)
+admin.site.register(Roles)
+admin.site.register(RolesRaisers)
+admin.site.register(SpaceAchiv)
+admin.site.register(Users)
+
--- django/DiplomDjango/DiplomDjangoCFDB/views.py
+++ django/DiplomDjango/DiplomDjangoCFDB/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseNotFound
 
-# Create your views here.
+class database:
+
+    def main(request):
+        return HttpResponse("База данных")
+    def get_role_database(request, role_database):
+        role_databaseS = {
+        'database':'База данных для отображения пользователю',
+        'moderation_database':'База данных для отображения модератору',
+        'opration_database':'База данных для отображения оператору БД',
+        'admin_database':'База данных для отображения админу',
+        }
+        content = role_databaseS.get(role_database)
+        
+        if content:
+            return HttpResponse(content)
+        else:
+            return HttpResponseNotFound(f"Неизвестная база данных - {role_database}")
+
